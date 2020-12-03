0aaf946a83e6a134095b6d15b0c81487c7a3c12f
Sharvin <sharvintheju@gmail.com>
2020-12-03 23:28:48 +0530

    Done with backend dev to mongo with pusher

---
+++ server.js
@@ -0,0 +1,115 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import mongoData from "./mongoData.js";
+import Pusher from "pusher";
+import dotenv from "dotenv";
+dotenv.config();
+// api config
+const app = express();
+const port = process.env.PORT || 8002;
+
+const pusher = new Pusher({
+  appId: "1117286",
+  key: "c8712fa6f2e7ca43a458",
+  secret: "f2ce7818dbd0fd0d2de9",
+  cluster: "ap2",
+  useTLS: true,
+});
+
+// middlewaare
+app.use(express.json());
+app.use(cors());
+
+// db config
+mongoose.connect(process.env.DB_CONNECTION, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("db connected");
+  const changeStream = mongoose.connection.collection("conversations").watch();
+  changeStream.on("change", (change) => {
+    if (change.operationType === "insert") {
+      console.log("new channel");
+      pusher.trigger("channels", "newChannel", {
+        change: change,
+      });
+    } else if (change.operationType === "update") {
+      pusher.trigger("conversation", "newMessage", {
+        change: change,
+      });
+    } else {
+      console.log("err in pusher");
+    }
+  });
+});
+// s5yjDMLBNZQ9hSOl
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+app.post("/new/channel", (req, res) => {
+  const dbData = req.body;
+  mongoData.create(dbData, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+app.get("/get/channelList", (req, res) => {
+  mongoData.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      let channels = [];
+      data.map((channelData) => {
+        const channelInfo = {
+          id: channelData._id,
+          name: channelData.channelName,
+        };
+        channels.push(channelInfo);
+      });
+      res.status(200).send(channels);
+    }
+  });
+});
+app.post("/new/message", (req, res) => {
+  const newMessage = req.body;
+  mongoData.update(
+    { _id: req.query.id },
+    { $push: { conversation: newMessage } },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    }
+  );
+});
+
+app.get("/get/data", (req, res) => {
+  mongoData.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get("/get/conversation", (req, res) => {
+  const id = req.query.id;
+  mongoData.find({ _id: id }, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+// listener
+app.listen(port, () => console.log(`listening on localhost :${port}`));
